42021af9577c8e3eff8edbf81e5024a5daefee91
André César <andrecesarhenrique@gmail.com>
2025-06-12 20:58:48 -0300

    entrada do nome do usuario

--- App.java
+++ App.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("hello mundo!");
+
+        System.out.println("digite seu nome: ");
+        String nome = sc.nextLine();
+
+        sc.close();
+
+        System.out.println("Bem vindo (a), " + nome + "!");
+
         
     }
 }
