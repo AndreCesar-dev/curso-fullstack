203b0d7758dbbcfcadc976717aebcfb3a1467179
André César <andrecesarhenrique@gmail.com>
2025-06-15 12:31:54 -0300

    adicinando mais uma linda de codigo para testar se fixei o conteudo

--- projeto2-java/src/App.java
+++ projeto2-java/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Me chamo André César e estou mergulhando no universo da tecnologia");
+        System.out.println("hoje estudei por mais ou menos 2 horas git e github aprendi e fixei varios comandos");
+        
     }
 }
