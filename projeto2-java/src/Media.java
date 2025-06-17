9ba6db6212547c56c34bb7ffcd784393053a3a65
André César <andrecesarhenrique@gmail.com>
2025-06-17 20:37:05 -0300

    melhorando o nome das variaveis

--- projeto2-java/src/Media.java
+++ projeto2-java/src/Media.java
@@ -5,17 +5,17 @@ public class Media {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("digite o valor de x: ");
-        int x = sc.nextInt();
+        int n1 = sc.nextInt();
 
         System.out.println("digite o valor de y: ");
-        int y = sc.nextInt();
+        int n2 = sc.nextInt();
 
         System.out.println("digite o valor de z: ");
-        int z = sc.nextInt();
+        int n3 = sc.nextInt();
 
         sc.close();
 
-        double media = (x + y + z)  / 2.0;
+        double media = (n1 + n2 + n3)  / 2.0;
 
         System.out.printf("A media de x, y e z é: %.2f\n", media );
     }
